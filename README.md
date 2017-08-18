7d856e63187d0b12332a5a3a587d1491c63d35ec
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-08-18 14:20:55 -0300

    Initial Release

---
+++ README.md
@@ -0,0 +1,104 @@
+# USDocker Generic LEMP Server
+
+This Useful Script creates a really generic NGINX+PHP+MYSQL server from a Docker image.
+You don't know docker to use this solution.  
+
+The server will be recognize the domain and select dynamically the folder where this domain will be serve pages.
+
+The default ROOT for the server is:
+
+```
+$HOME/.usdocker/data/lemp/sites
+```
+
+## Installing
+
+```bash
+npm install -g usdocker-lemp
+```
+
+## Running
+
+```bash
+usdocker mysql up    # installed as dependency
+usdocker lemp up
+```
+
+Check the status
+
+```bash
+usdocker lemp status
+```
+
+Down the server
+
+```bash
+usdocker lemp down
+```
+
+## Serving domains
+
+If you wanna serve pages for domain "example.com" you just have to create a folder:
+
+```bash
+usdocker lemp domain-add example.com
+```
+
+And you will serve pages for this domain immediatelly. 
+This script tries to find the follow directories to server web pages:
+
+- web
+- httpdocs
+- public 
+
+If found, the directory for the web pages will be, respectively:
+
+```
+$HOME/.usdocker/data/lemp/sites/example.com/web
+or
+$HOME/.usdocker/data/lemp/sites/example.com/httpdocs
+or
+$HOME/.usdocker/data/lemp/sites/example.com/public
+```
+
+If not found will serve the root directory directly
+
+### Serve a domain with a previous specific content
+
+Optionally, you can create a domain with a pre-defined content existing in a tar.gz file. 
+ 
+To do this execute:
+
+```bash
+usdocker lemp domain-add example.com /path/to/file.tar.gz
+```
+
+### Remove a domain
+
+```bash
+usdocker lemp domain-del example.com
+```
+
+### List served domains
+
+```bash
+usdocker lemp domain-list
+```
+
+## Customize your Service
+
+You can setup the variables by using:
+
+```bash
+usdocker lemp --set variable=value
+```
+
+Default values
+
+ - image: "byjg/php7-fpm-nginx:alpine",
+ - folder: "/home/jg/.usdocker/data/lemp",
+ - port: 80,
+ - sslPort: 443,
+ - applicationEnv: "dev" (will set APPLICATION_ENV environment variable to PHP)
+
+
---
+++ usdocker_lemp.js
@@ -0,0 +1,141 @@
+'use strict';
+
+const usdocker = require('usdocker');
+const fs = require('fs');
+const path = require('path');
+const targz = require('targz');
+const fsutil = usdocker.fsutil();
+
+const SCRIPTNAME = 'lemp';
+
+let config = usdocker.config(SCRIPTNAME);
+let configGlobal = usdocker.configGlobal();
+
+function getContainerDef() {
+
+    let docker = usdocker.dockerRunWrapper(configGlobal);
+    return docker
+        .containerName(SCRIPTNAME + configGlobal.get('container-suffix'))
+        .port(config.get('port'), 80)
+        .port(config.get('sslPort'), 443)
+        .volume(config.get('folder') + '/sites', '/srv/web')
+        .volume(config.getUserDir('fpm') + '/fpmpool/zz-docker-2.conf', '/usr/local/etc/php-fpm.d/zz-docker-2.conf')
+        .volume(config.getUserDir('fpm') + '/php/custom.ini', '/usr/local/etc/php/conf.d/custom.ini')
+        .volume(config.getUserDir('nginx') + '/conf.d', '/etc/nginx/conf.d/')
+        .env('LEMP_DATA_FOLDER', config.get('folder'))
+        .env('LEMP_PORT', config.get('port'))
+        .env('LEMP_SSL_PORT', config.get('sslPort'))
+        .env('APPLICATION_ENV', config.get('applicationEnv'))
+        .env('TZ', configGlobal.get('timezone'))
+        .isDetached(true)
+        .isRemove(true)
+        .imageName(config.get('image'))
+    ;
+}
+
+module.exports = {
+    setup: function(callback)
+    {
+        config.setEmpty('image', 'byjg/php7-fpm-nginx:alpine');
+        config.setEmpty('folder', config.getDataDir());
+        config.setEmpty('port', 80);
+        config.setEmpty('sslPort', 443);
+        config.setEmpty('applicationEnv', 'dev');
+
+        config.copyToUserDir(__dirname + '/lemp/conf/fpm');
+        config.copyToUserDir(__dirname + '/lemp/conf/nginx');
+
+        config.copyToDataDir(__dirname + '/lemp/sites');
+
+        callback(null, 'setup loaded for ' + SCRIPTNAME);
+    },
+
+    domainAdd(callback, extraArgs) {
+        if (extraArgs.length !== 1 && extraArgs.length !== 2) {
+            throw new Error('domain-add requires two parameters: domain and template (optional .tar.gz file)');
+        }
+
+        let destFolder = path.join(config.get('folder'), 'sites', extraArgs[0]);
+        if (fs.existsSync(destFolder)) {
+            throw new Error('Domain "' + extraArgs[0] + '" already created');
+        }
+
+        if (extraArgs.length === 1) {
+            let sourceFolder = path.join(config.get('folder'), 'sites', '.template');
+            fsutil.copyFolderRecursiveSync(sourceFolder, destFolder);
+            callback(null, 'Created domain "' + extraArgs[0] + '" from template');
+        }
+
+        if (extraArgs.length === 2) {
+            targz.decompress({
+                src: extraArgs[1],
+                dest: destFolder
+            }, function(err){
+                if(err) {
+                    callback(err);
+                } else {
+                    callback(null, 'Created domain "' + extraArgs[0] + '" from "' + extraArgs[1] + '"');
+                }
+            });
+        }
+    },
+
+    domainDel(callback, extraArgs) {
+        if (extraArgs.length !== 1) {
+            throw new Error('domain-del requires one parameter: domain');
+        }
+
+        let destFolder = path.join(config.get('folder'), 'sites', extraArgs[0]);
+        if (!fs.existsSync(destFolder)) {
+            throw new Error('Domain "' + extraArgs[0] + '" does not exists');
+        }
+
+        usdocker.ask(
+            'Are you sure you want to continue',
+            false,
+            this.options.yes,
+            this.options.no,
+            function() {
+                fsutil.removeDirectoryRecursive(destFolder);
+                callback(null, 'Domain removed!');
+            },
+            callback
+        );
+    },
+
+    domainList(callback) {
+        let destFolder = path.join(config.get('folder'), 'sites');
+        let result = fsutil.getDirectories(destFolder);
+
+        callback(result.join('\n'), 'Domain list:\n - ' + result.join('\n - '));
+    },
+
+    debugcli(callback) {
+        let result = usdocker.outputRaw('cli', getContainerDef());
+        callback(result);
+    },
+
+    debugapi(callback) {
+        let result = usdocker.outputRaw('api', getContainerDef());
+        callback(result);
+    },
+
+    up: function(callback)
+    {
+        usdocker.up(SCRIPTNAME, getContainerDef(), callback);
+    },
+
+    status: function(callback) {
+        usdocker.status(SCRIPTNAME, callback);
+    },
+
+    down: function(callback)
+    {
+        usdocker.down(SCRIPTNAME, callback);
+    },
+
+    restart: function(callback)
+    {
+        usdocker.restart(SCRIPTNAME, getContainerDef(), callback);
+    }
+};
